a476465971b300ae5c56b4a133682c360cfd909d
ryan3770 <aryan05jan@gmail.com>
2017-12-21 12:52:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,10 @@ client.on('message', message => {
     message.channel.send('Pong :ping_pong:'); 
   }
 });
+
+exports.run = (client, message, args) => {
+    let argss = message.content.split(" ").slice(1);
+    let tempmsg = argss.join(" ");
+    message.delete();
+    return message.channel.send(tempmsg);
+}
