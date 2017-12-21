68ac88086ad8ee4e8d256e944aafcb8d6c7d3e2d
ryan3770 <aryan05jan@gmail.com>
2017-12-21 18:45:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,23 +36,3 @@ console.error(err);
 }
 
 }
-
-client.reload = command => { 
-  return new Promise((resolve, reject) => {
-    try { 
-      delete require.cache[require.resolve(`./commands/${command}`)];
-      let cmd = require(`./commands/${command}`);
-      client.commands.delete(command);
-      client.aliases.forEach((cmd, alias) => {
-        if (cmd === command) client.aliases.delete(alias);
-      });
-      client.commands.set(command, cmd);
-      cmd.conf.aliases.forEach(alias => {
-        client.aliases.set(alias, cmd.help.name);
-      });
-      resolve();
-    } catch (e){
-      reject(e);
-    }
-  });
-};
