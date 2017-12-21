0f1876ef908851d143ba194899e7fb53d39d8634
ryan3770 <aryan05jan@gmail.com>
2017-12-21 12:42:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
-const config = require('./config.json');
 const prefix = "!";
 const botownerid = "280749589974482945";
 
