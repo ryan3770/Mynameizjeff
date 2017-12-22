32ef00eb17571dc654fbef430ad9026836bf3534
ryan3770 <aryan05jan@gmail.com>
2017-12-22 12:32:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,3 +30,10 @@ let psymsg = sigh.join(" ");
 message.channel.send('Reported!');
 client.users.get(config.botownerid).send(`${message.author.tag} from ${message.guild.name} have reported: ` + psymsg);
 }
+
+exports.run = (client, message, args) => {
+    let argss = message.content.split(" ").slice(1);
+    let tempmsg = argss.join(" ");
+    message.delete();
+    return message.channel.send(tempmsg);
+}
